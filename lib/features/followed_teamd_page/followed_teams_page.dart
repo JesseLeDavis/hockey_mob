6f513c6cb3890efa2b662efb535a6d0354db6daf
JesseLeDavis <96789552+JesseLeDavis@users.noreply.github.com>
2024-02-10 13:25:57 -0500

    following page ui

--- lib/features/followed_teamd_page/followed_teams_page.dart
+++ lib/features/followed_teamd_page/followed_teams_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_project_template_firebase/app/view/app.dart';
 import 'package:flutter_project_template_firebase/constants/gaps.dart';
 import 'package:flutter_project_template_firebase/infrastructure/injection/injection.dart';
 import 'package:flutter_project_template_firebase/persistance/hive_data_store.dart';
+import 'package:flutter_project_template_firebase/shared/themes/color_themes.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FollowedTeamsPage extends StatelessWidget {
   const FollowedTeamsPage({super.key});
@@ -20,6 +22,8 @@ class FollowedTeamsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _borderRadius = BorderRadius.circular(12);
+
     return Scaffold(
       appBar: AppBar(
         title: const Row(
@@ -35,25 +39,62 @@ class FollowedTeamsView extends StatelessWidget {
         valueListenable: getIt<HiveDataStore>().teamsListenable(),
         builder: (_, box, __) {
           final boxList = box.values.toList();
-          return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              crossAxisCount: 2,
-            ),
-            itemCount: boxList.length,
-            itemBuilder: (context, index) {
-              final team = boxList[index];
-              return Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                crossAxisCount: 2,
+              ),
+              itemCount: boxList.length,
+              itemBuilder: (context, index) {
+                final team = boxList[index];
+                return Material(
+                  borderRadius: _borderRadius,
                   color: Color(int.parse(team.primaryColor)),
-                ),
-                child: Row(
-                  children: [Text(team.teamName)],
-                ),
-              );
-            },
+                  child: InkWell(
+                    borderRadius: _borderRadius,
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        borderRadius: _borderRadius,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Row(
+                            children: [
+                              SvgPicture.asset(team.largeLogo),
+                            ],
+                          ),
+                          const Spacer(),
+                          Row(
+                            children: [
+                              const Spacer(
+                                flex: 40,
+                              ),
+                              Expanded(
+                                flex: 80,
+                                child: Text(
+                                  team.teamName,
+                                  style: context.headline,
+                                  textAlign: TextAlign.right,
+                                ),
+                              ),
+                            ],
+                          ),
+                          gapH4,
+                          const Text('vs Some Team'),
+                          const Text('at Dec 15 @ 3:30'),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           );
         },
       ),
